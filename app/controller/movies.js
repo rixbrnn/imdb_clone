a2ae6052f5cfbd21ed0430fb8596adffada02923
cbueno <cgbueno@grupopanvel.com.br>
2022-04-29 12:43:52 -0300

    Add labels to show movie on page

--- app/controller/movies.js
+++ app/controller/movies.js
@@ -1,7 +1,23 @@
 axios.get('http://localhost:3000/movies')
 .then(resp => {
-    console.log(resp.data);
+    var movies = resp.data;
+    movies.forEach(movie => {
+        displayMovies(movie)
+    });
 })
 .then(err => {
     console.log(err)
 })
+
+function displayMovies(movie) {
+    document.getElementById('idId').innerText = movie.id
+    document.getElementById('idTitle').innerText = movie.title
+    document.getElementById('idGenres').innerText = movie.genre_ids[0]
+    document.getElementById('idDescription').innerText = movie.description
+    document.getElementById('idImageUrl').innerText = movie.image_url
+    document.getElementById('idYoutubeUrl').innerText = movie.youtube_url
+    document.getElementById('idPegi').innerText = movie.pegi
+    document.getElementById('idReleaseDate').innerText = movie.release_date
+    document.getElementById('idCreationUser').innerText = movie.creation_user
+    document.getElementById('idCreationDate').innerText = movie.creation_date
+}
