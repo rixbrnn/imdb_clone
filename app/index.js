3c9fe09cc8f88225305a28c6b72b461453bb9369
Richard Bringmann <richardbringmann160301@gmail.com>
2022-04-29 15:58:52 -0300

    fix project structure

--- app/index.js
+++ app/index.js
@@ -1,27 +1,31 @@
-const headerTemplate = document.createElement("node");
-const footerTemplate = document.createElement("node");
+function generateHeaderAndFooter(){
+    const headerTemplate = document.createElement("node");
+    const footerTemplate = document.createElement("node");
+    
+    headerTemplate.innerHTML = `
+    <div id="box">  
+        <h2 id="page-title"> IMDb </h2>
+        <nav>
+            <ul>
+                <li><a href="/app/index.html">HOME</a></li>
+                <li><a href="/app/view/movies.html">MOVIES</a></li>
+                <li><a href="/app/view/add_movie.html">ADD MOVIE</a></li>
+                <li><a href="/app/view/reviews.html">REVIEWS</a></li>
+                <li><a href="/app/view/login.html">LOGIN</a></li>
+            </ul>
+        </nav>
+    </div>
+    `;
+    
+    footerTemplate.innerHTML = `
+        <h2> Desenvolvimento para a web - Unisinos </h2>
+    `;
+    
+    document.querySelector("header").appendChild(headerTemplate);
+    document.querySelector("footer").appendChild(footerTemplate);
+}
 
-headerTemplate.innerHTML = `
-<div id="box">  
-    <h2 id="page-title"> IMDb </h2>
-    <nav>
-        <ul>
-            <li><a href="/app/index.html">HOME</a></li>
-            <li><a href="/app/view/movies.html">MOVIES</a></li>
-            <li><a href="/app/view/add_movie.html">ADD MOVIE</a></li>
-            <li><a href="/app/view/reviews.html">REVIEWS</a></li>
-            <li><a href="/app/view/login.html">LOGIN</a></li>
-        </ul>
-    </nav>
-</div>
-`;
 
-footerTemplate.innerHTML = `
-    <h2> Desenvolvimento para a web - Unisinos </h2>
-`;
-
-document.querySelector("header").appendChild(headerTemplate);
-document.querySelector("footer").appendChild(footerTemplate);
 
 /* exemplo de uso do Axios:
 axios.post('http://localhost:3000/genres', {"foo":"bar"}).then(resp => {
