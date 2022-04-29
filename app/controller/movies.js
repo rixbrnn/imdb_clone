0b365b25c5c13de9750449e5439ac45b43ac5ad4
cbueno <cgbueno@grupopanvel.com.br>
2022-04-29 10:07:12 -0300

    Import database and get movies

--- app/controller/movies.js
+++ app/controller/movies.js
@@ -1,12 +1,7 @@
-const axios = require('../json-server-lib/node_modules/axios');
-
 axios.get('http://localhost:3000/movies')
 .then(resp => {
-    data = resp.data;
-data.forEach(e => {
-    console.log(`${e.title}`);
-});
+    console.log(resp.data);
 })
-.catch(error => {
-    console.log(error);
-});
+.then(err => {
+    console.log(err)
+})
