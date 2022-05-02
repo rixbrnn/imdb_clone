2d181e316237c47750a6e9ff417834373451d1e9
cbueno <cgbueno@grupopanvel.com.br>
2022-05-01 22:30:58 -0300

    Add - function to add movie on Database

--- app/controller/addMovies.js
+++ app/controller/addMovies.js
@@ -16,5 +16,21 @@ function verifyLastId(movies) {
 getLastId()
 
 function postMovie() {
-    alert("submit")
+    axios.post('http://localhost:3000/movies', 
+        {
+            "id":`${document.getElementById("id").value}`,
+            "title":`${document.getElementById("title").value}`,
+            "genre_ids":`${document.getElementById("genre").value}`,
+            "description":`${document.getElementById("description").value}`,
+            "image_url":`${document.getElementById("image_url").value}`,
+            "youtube_url":`${document.getElementById("youtube_url").value}`,
+            "pegi":`${document.getElementById("pegi").value}`,
+            "release_date":`${document.getElementById("release_date").value}`,
+            "creation_date":`${document.getElementById("creation_date").value}`,
+            "creation_user":`${document.getElementById("creation_user").value}`,
+            "director":`${document.getElementById("director").value}`
+        }).then(resp => {
+            console.log(resp.data);
+            window.location.reload()
+        });
 }
