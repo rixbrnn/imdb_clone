e6daa8bbcdcdfb530c41255215aba54959863eb7
cbueno <cgbueno@grupopanvel.com.br>
2022-05-03 22:31:20 -0300

    Add - Css to addMovies and map username

--- app/controller/addMovies.js
+++ app/controller/addMovies.js
@@ -16,6 +16,12 @@ function verifyLastId(movies) {
 getLastId()
 
 function postMovie() {
+    const user = localStorage.getItem("currentuser"); 
+    if (!user) {
+        alert("Usuário não logado")
+        return
+    }
+    alert(`usuario ${user}`)
     axios.post('http://localhost:3000/movies', 
         {
             "id":`${document.getElementById("id").value}`,
@@ -27,7 +33,7 @@ function postMovie() {
             "pegi":`${document.getElementById("pegi").value}`,
             "release_date":`${document.getElementById("release_date").value}`,
             "creation_date":`${document.getElementById("creation_date").value}`,
-            "creation_user":`${document.getElementById("creation_user").value}`,
+            "creation_user":`${user}`,
             "director":`${document.getElementById("director").value}`
         }).then(resp => {
             console.log(resp.data);
