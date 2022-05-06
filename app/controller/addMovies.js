108f2332ff386eefe37d302a088e3adcab2e984e
cbueno <cgbueno@grupopanvel.com.br>
2022-05-06 11:32:27 -0300

    Refactor - Redirect to list films when add a new movie

--- app/controller/addMovies.js
+++ app/controller/addMovies.js
@@ -55,7 +55,7 @@ function postMovie() {
             "director":`${document.getElementById("director").value}`
         }).then(resp => {
             console.log(resp.data);
-            window.location.reload()
+            window.location="./movies.html";
         });
 }
 
